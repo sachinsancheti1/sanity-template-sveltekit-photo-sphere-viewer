085df85f2becbcee94f8f26dc5ff8757e924ad0c
Sachin Sancheti <sachin.sancheti@gmail.com>
2024-12-25 15:34:06 +0530

    fix in error
    - error showed 'Cannot use import statement outside a module sveltekit' . Fixed using the noExternal option in vite https://github.com/sveltejs/kit/issues/4504

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	ssr: {
+		noExternal: ['@photo-sphere-viewer/core', '@photo-sphere-viewer/gallery-plugin', '@photo-sphere-viewer/virtual-tour-plugin']
+	}
 });
